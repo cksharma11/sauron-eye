4e932f91cdd45b12e613e087d8ff5d713e36eddc
Chandan Kumar <cksharma122@gmail.com>
2019-12-16 00:52:51 +0530

    Updates

---
+++ precache-manifest.36fdd8f8c2043c02705614ec24cf3b2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d90c359606005e69520657069ba2c48",
+    "url": "/sauron-eye/index.html"
+  },
+  {
+    "revision": "5386bf93c9aa35019def",
+    "url": "/sauron-eye/static/css/main.e8fc28f1.chunk.css"
+  },
+  {
+    "revision": "c39c1d0e0e4e80cf51c1",
+    "url": "/sauron-eye/static/js/2.4b930584.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/sauron-eye/static/js/2.4b930584.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5386bf93c9aa35019def",
+    "url": "/sauron-eye/static/js/main.fe120fba.chunk.js"
+  },
+  {
+    "revision": "33a6f014dcd832b57517",
+    "url": "/sauron-eye/static/js/runtime-main.123eb616.js"
+  }
+]);
